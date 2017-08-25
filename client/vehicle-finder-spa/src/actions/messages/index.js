695a12b5d6327fba3786ef89b5d14635f35329c4
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2017-08-25 22:48:12 +0300

    adding messages actions and reducer

---
+++ client/vehicle-finder-spa/src/actions/messages/index.js
@@ -0,0 +1,259 @@
+export const ALL_MESSAGE_THREADS_GET_FETCHING = 'MESSAGE_THREADS_GET_FETCHING'
+export const ALL_MESSAGE_THREADS_GET_SUCCESS = 'ALL_MESSAGE_THREADS_GET_SUCCESS'
+export const ALL_MESSAGE_THREADS_GET_ERROR = 'ALL_MESSAGE_THREADS_GET_ERROR'
+
+export const MESSAGE_THREAD_POST_FETCHING = 'MESSAGE_THREAD_POST_FETCHING'
+export const MESSAGE_THREAD_POST_SUCCESS = 'MESSAGE_THREAD_POST_SUCCESS'
+export const MESSAGE_THREAD_POST_ERROR = 'MESSAGE_THREAD_POST_ERROR'
+
+export const ALL_MESSAGES_GET_FETCHING = 'ALL_MESSAGES_GET_FETCHING'
+export const ALL_MESSAGES_GET_SUCCESS = 'ALL_MESSAGES_GET_SUCCESS'
+export const ALL_MESSAGES_GET_ERROR = 'ALL_MESSAGES_GET_ERROR'
+
+export const MESSAGE_PUT_FETCHING = 'MESSAGE_PUT_FETCHING'
+export const MESSAGE_PUT_SUCCESS = 'MESSAGE_PUT_SUCCESS'
+export const MESSAGE_PUT_ERROR = 'MESSAGE_PUT_ERROR'
+
+export const MESSAGE_READ_FETCHING = 'MESSAGE_READ_FETCHING'
+export const MESSAGE_READ_SUCCESS = 'MESSAGE_READ_SUCCESS'
+export const MESSAGE_READ_ERROR = 'MESSAGE_READ_ERROR'
+
+/**
+ * Message threads get
+ */
+
+function allMessageThreadsGetFetching() {
+    return {
+        type: ALL_MESSAGE_THREADS_GET_FETCHING
+    }
+}
+
+function allMessageThreadsGetSuccess(messageThreads) {
+    return {
+        type: ALL_MESSAGE_THREADS_GET_SUCCESS,
+        payload: { messageThreads: messageThreads }
+    }
+}
+
+function allMessageThreadsGetError(msg) {
+    return {
+        type: ALL_MESSAGE_THREADS_GET_SUCCESS,
+        payload: { message: msg }
+    }
+}
+
+function requestAllMessageThreads() {
+    return dispatch => {
+        dispatch(allMessageThreadsGetFetching())
+        return fetch(`http://localhost:3000/messages`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include'
+            })
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(json => {
+                console.log(json);
+                if (json.success) {
+                    dispatch(allMessageThreadsGetSuccess(json.threads))
+                } else {
+                    dispatch(allMessageThreadsGetError(json.msg))
+                }                
+            })
+        
+    }
+}
+
+/**
+ * Message threads post
+ */
+
+function messageThreadPostFetching() {
+    return {
+        type: MESSAGE_THREAD_POST_FETCHING
+    }
+}
+
+function messageThreadPostSuccess(messageThread, msg) {
+    return {
+        type: MESSAGE_THREAD_POST_SUCCESS,
+        payload: { messageThread: messageThread, message: msg }
+    }
+}
+
+function messageThreadPostError(msg) {
+    return {
+        type: MESSAGE_THREAD_POST_ERROR,
+        payload: { message: msg }
+    }
+}
+
+function createMessageThread(messageThread) {
+    return dispatch => {
+        dispatch(messageThreadPostFetching())
+        return fetch(`http://localhost:3000/messages/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
+                body: JSON.stringify(messageThread)
+            })
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(json => {
+                console.log(json);
+                if(json.success) {
+                    dispatch(messageThreadPostSuccess(json.thread, json.msg))
+                } else {
+                    dispatch(messageThreadPostError(json.msg))
+                }                
+            })
+    }
+}
+
+/**
+ * All messages in thread get
+ */
+
+function allMessagesGetFetching() {
+    return {
+        type: ALL_MESSAGES_GET_FETCHING
+    }
+}
+
+function allMessagesGetSuccess(messages) {
+    return {
+        type: ALL_MESSAGE_THREADS_GET_SUCCESS,
+        payload: { messages: messages }
+    }
+}
+
+function allMessagesGetError() {
+    return {
+        type: ALL_MESSAGE_THREADS_GET_ERROR
+    }
+}
+
+function requestAllMessages(messageThread) {
+    return dispatch => {
+        dispatch(allMessagesGetFetching())
+        return fetch(`http://localhost:3000/messages/${messageThread._id}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include'
+            })
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(json => {
+                console.log(json);
+                if(json.success) {
+                    dispatch(allMessagesGetSuccess(json.thread))
+                } else {
+                    dispatch(allMessagesGetError())
+                }                
+            })
+    }
+}
+
+/**
+ * Message put
+ */
+
+function messagePutFetching() {
+    return {
+        type: MESSAGE_PUT_FETCHING
+    }
+}
+
+function messagePutSuccess(chatMessage, msg) {
+    return {
+        type: MESSAGE_PUT_SUCCESS,
+        payload: {chatMessage: chatMessage, message: msg}
+    }
+}
+
+function messagePutError(msg) {
+    return {
+        type: MESSAGE_PUT_ERROR,
+        payload: {message: msg}
+    }
+}
+
+function sendMessage(message, messageThread) {
+     return dispatch => {
+        dispatch(messagePutFetching())
+        return fetch(`http://localhost:3000/api/messages/${messageThread._id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
+                body: JSON.stringify(message)
+            })
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(json => {
+                console.log(json);
+                if(json.success) {
+                    dispatch(messagePutSuccess(json.thread, json.msg))
+                } else {
+                    dispatch(messagePutError(json.msg))
+                }                
+            })
+    }
+}
+
+/**
+ * Message patch
+ */
+
+function messageReadFetching() {
+    return {
+        type: MESSAGE_READ_FETCHING
+    }
+}
+
+function messageReadSuccess(msg) {
+    return {
+        type: MESSAGE_READ_SUCCESS,
+        payload: { message: msg}
+    }
+}
+
+function messageReadError(msg) {
+    return {
+        type: MESSAGE_READ_ERROR,
+        payload: {message: msg}
+    }
+}
+
+function markAsRead(messageThread) {
+     return dispatch => {
+        dispatch(messagePutFetching())
+        return fetch(`http://localhost:3000/api/messages/${messageThread._id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include'
+            })
+            .then(response => {
+                console.log(response);
+                return response.json()
+            })
+            .then(json => {
+                console.log(json);
+                if(json.success) {
+                    dispatch(messagePutSuccess(json.msg))
+                } else {
+                    dispatch(messagePutError(json.msg))
+                }                
+            })
+    }
+}
+
+export { requestAllMessageThreads, createMessageThread, requestAllMessages, 
+        sendMessage, markAsRead}
---
+++ client/vehicle-finder-spa/src/reducers/messages.js
@@ -0,0 +1,81 @@
+import {
+    ALL_MESSAGE_THREADS_GET_FETCHING,
+    ALL_MESSAGE_THREADS_GET_SUCCESS,
+    ALL_MESSAGE_THREADS_GET_ERROR,
+    MESSAGE_THREAD_POST_FETCHING,
+    MESSAGE_THREAD_POST_SUCCESS ,
+    MESSAGE_THREAD_POST_ERROR,
+    ALL_MESSAGES_GET_FETCHING,
+    ALL_MESSAGES_GET_SUCCESS,
+    ALL_MESSAGES_GET_ERROR,
+    MESSAGE_PUT_FETCHING,
+    MESSAGE_PUT_SUCCESS,
+    MESSAGE_PUT_ERROR,
+    MESSAGE_READ_FETCHING,
+    MESSAGE_READ_SUCCESS,
+    MESSAGE_READ_ERROR
+} from '../actions/messages'
+
+const defaultState = {
+    sendUser: '',
+    recieveUser: '',
+    cuncernedOffer: '',
+    messages: [],
+    senderLastSeen: '',
+    recieverLastSeen: '',
+    updatedAt: '',
+    createdAt: '',
+    isFetching: false,
+    currentInfoMessage: '',
+    currentErrorMessage: '',
+    messageThreads: []
+}
+
+const messages = function (state = defaultState, action) {
+    switch(action.type) {
+        case ALL_MESSAGE_THREADS_GET_FETCHING:
+            return {...state, isFetching: true, currentErrorMessage: '', currentInfoMessage: ''}
+        case ALL_MESSAGE_THREADS_GET_SUCCESS:
+            return {...state, isFetching: false, messageThreads: action.payload.messageThreads}
+        case ALL_MESSAGE_THREADS_GET_ERROR:
+            return {...state, isFetching: false, currentErrorMessage: action.payload.message}
+        
+        case MESSAGE_THREAD_POST_FETCHING:
+            return {...state, isFetching: true, currentErrorMessage: '', currentInfoMessage: ''}
+        case MESSAGE_THREAD_POST_SUCCESS:
+            return {...state, isFetching: false, currentInfoMessage: action.payload.message, 
+                    messageThreads: addElementToArray(state.messageThreads, action.payload.messageThread)}
+        case MESSAGE_THREAD_POST_ERROR:
+            return {...state, isFetching: false, currentErrorMessage: action.payload.message}
+
+        case ALL_MESSAGES_GET_FETCHING:
+            return {...state, isFetching: true, currentErrorMessage: '', currentInfoMessage: ''}
+        case ALL_MESSAGES_GET_SUCCESS:
+            return {...state, isFetching: false, currentInfoMessage: action.payload.message, 
+                    messages: action.payload.messages}
+        case ALL_MESSAGES_GET_ERROR:
+            return {...state, isFetching: false}
+
+        case MESSAGE_PUT_FETCHING:
+            return {...state, isFetching: true, currentErrorMessage: '', currentInfoMessage: ''}
+        case MESSAGE_PUT_SUCCESS:
+            return {...state, isFetching: false, currentInfoMessage: action.payload.message, 
+                    messages: addElementToArray(state.messages, action.payload.chatMessage)}
+        case MESSAGE_PUT_ERROR:
+            return {...state, isFetching: false, currentErrorMessage: action.payload.message}
+        
+        case MESSAGE_READ_FETCHING:
+            return {...state, isFetching: true, currentErrorMessage: '', currentInfoMessage: ''}
+        case MESSAGE_READ_SUCCESS:
+            return {...state, isFetching: false, currentInfoMessage: action.payload.message}
+        case MESSAGE_READ_ERROR:
+            return {...state, isFetching: false, currentErrorMessage: action.payload.message}
+    }
+}
+
+function addElementToArray(arr, messageThread)
+{
+    const newArr = arr.spice()
+    newArr.push(messageThread)
+    return newArr
+}
